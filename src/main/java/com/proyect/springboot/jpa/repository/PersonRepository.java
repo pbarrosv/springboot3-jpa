ca72441173146604505a0f66fb629103f97026bc
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-04-12 13:19:07 -0500

    Creacion de PersonRepository y metodo personalizados con @Query

--- src/main/java/com/proyect/springboot/jpa/repository/PersonRepository.java
+++ src/main/java/com/proyect/springboot/jpa/repository/PersonRepository.java
@@ -1,9 +1,21 @@
 package com.proyect.springboot.jpa.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.proyect.springboot.jpa.entity.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+	List<Person> findByProgramingLanguage(String programingLanguage);
+	
+	@Query("select p from Person p where p.programingLanguage=?1 and p.name=?2")
+	List<Person> BuscarByProgramingLanguage(String programingLanguage, String name);
+	
+	List<Person> findByProgramingLanguageAndName(String programingLanguage, String name);
+	
+	@Query("select p.name, p.programingLanguage from Person p")
+	List<Object[]> obtenerPersonData();
 }
