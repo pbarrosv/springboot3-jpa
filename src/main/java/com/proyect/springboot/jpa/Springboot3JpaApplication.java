7da88abf5117ba30b4f0fa57be1d066ce9617082
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-04-11 20:10:11 -0500

    Implement CommandLineRunner

--- src/main/java/com/proyect/springboot/jpa/Springboot3JpaApplication.java
+++ src/main/java/com/proyect/springboot/jpa/Springboot3JpaApplication.java
@@ -1,13 +1,20 @@
 package com.proyect.springboot.jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot3JpaApplication {
+public class Springboot3JpaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3JpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
