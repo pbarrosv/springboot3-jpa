a5d8f61a332992433da7757048396e2275afdb70
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-04-15 11:18:03 -0500

    Creacion del metodo JPQL getNameById() en la interface PersonRepository

--- src/main/java/com/proyect/springboot/jpa/repository/PersonRepository.java
+++ src/main/java/com/proyect/springboot/jpa/repository/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.proyect.springboot.jpa.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,6 +10,26 @@ import com.proyect.springboot.jpa.entity.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+	/******JPQL campos personalizados***********/
+	@Query("select p.name from Person p where p.id=?1")
+	String getNameBy();
+	
+	
+	
+	/*******************************************/
+	@Query("select p from Person p where p.id=?1")
+	Optional<Person> findOne(Long id);
+	
+	@Query("select p from Person p where p.name=?1")
+	Optional<Person> findOneName(String name);
+	
+	@Query("select p from Person p where p.name like %?1%")
+	Optional<Person> findOneLikeName(String name);
+	
+	Optional<Person> findByName(String name);
+	//Containin hace un Like
+	Optional<Person> findByNameContaining(String name);
+	
 	List<Person> findByProgramingLanguage(String programingLanguage);
 	
 	@Query("select p from Person p where p.programingLanguage=?1 and p.name=?2")
@@ -18,4 +39,6 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
 	
 	@Query("select p.name, p.programingLanguage from Person p")
 	List<Object[]> obtenerPersonData();
+	
+	
 }
